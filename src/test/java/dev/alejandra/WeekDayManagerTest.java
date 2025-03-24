1885a93e5fd8833c3d93f9738358c5b8a0d8e983
Akalchi <alejandrafdez1699@gmail.com>
2025-03-24 13:27:12 +0100

    "TEST [ManagerHasEmptiList] failed (testing constructor)"

---
+++ src/test/java/dev/alejandra/WeekDayManagerTest.java
@@ -0,0 +1,22 @@
+package dev.alejandra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WeekDayManagerTest {
+
+    private WeekDayManager weekDayManager;
+    
+    @BeforeEach
+    public void setUp() {
+        weekDayManager = new WeekDayManager();
+    }
+
+    @Test
+    public void testNewManagerHasEmptyList() {
+        assertEquals(0, weekDayManager.getSize());
+    }
+
+}
