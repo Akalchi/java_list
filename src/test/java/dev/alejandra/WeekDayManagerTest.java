87eeed37efe41b24659dc365383b45ae5a7b5307
Akalchi <alejandrafdez1699@gmail.com>
2025-03-24 13:35:54 +0100

    "TEST [GetWeekDays] failed"

--- src/test/java/dev/alejandra/WeekDayManagerTest.java
+++ src/test/java/dev/alejandra/WeekDayManagerTest.java
@@ -2,6 +2,8 @@ package dev.alejandra;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +25,23 @@ public class WeekDayManagerTest {
     public void testCreateWeekDays() {
         weekDayManager.createWeekDays();
         assertEquals(7, weekDayManager.getSize());
+    }
+
+     @Test
+    public void testGetWeekDays() {
+        
+        weekDayManager.createWeekDays();
+        
+        List<String> weekDays = weekDayManager.getWeekDays();
+    
+        assertEquals(7, weekDays.size());
+        assertEquals("Monday", weekDays.get(0));
+        assertEquals("Tuesday", weekDays.get(1));
+        assertEquals("Wednesday", weekDays.get(2));
+        assertEquals("Thursday", weekDays.get(3));
+        assertEquals("Friday", weekDays.get(4));
+        assertEquals("Saturday", weekDays.get(5));
+        assertEquals("Sunday", weekDays.get(6));
     }
 
 }
